d5b2d5a3cebde13d78cfcd422004abac5e7c6577
aluis <andre_luis9214@hotmail.com>
2017-10-17 13:23:22 -0200

    feat: Add isSnakeCase function

--- src/caseChecker.js
+++ src/caseChecker.js
@@ -1,2 +1,5 @@
 export const isKebabCase = text =>
     /^[a-z]+(-[a-z]+)+$/g.test(text);
+
+export const isSnakeCase = text =>
+    /^[a-z]+(_[a-z]+)+$/g.test(text);
--- src/caseChecker.spec.js
+++ src/caseChecker.spec.js
@@ -1,5 +1,6 @@
 import {
     isKebabCase,
+    isSnakeCase,
 } from './caseChecker';
 
 describe('isKebabCase', () => {
@@ -27,3 +28,31 @@ describe('isKebabCase', () => {
         expect(isKebabCase('text-www-w-www')).toBeTruthy();
     });
 });
+
+
+describe('isSnakeCase', () => {
+    it('should be falsy if there is no _', () => {
+        expect(isSnakeCase('text')).toBeFalsy();
+    });
+
+    it('should be falsy if ends with _', () => {
+        expect(isSnakeCase('text_')).toBeFalsy();
+    });
+
+    it('should be falsy if starts with _', () => {
+        expect(isSnakeCase('_text')).toBeFalsy();
+    });
+
+    it('should be falsy if ends and starts with _', () => {
+        expect(isSnakeCase('_text_')).toBeFalsy();
+    });
+
+    it('should be falsy if there a space between letters', () => {
+        expect(isSnakeCase('text_ww w_w_www')).toBeFalsy();
+    });
+
+    it('should be truthy if there is _ between letters', () => {
+        expect(isSnakeCase('text_www_w_www')).toBeTruthy();
+    });
+});
+
